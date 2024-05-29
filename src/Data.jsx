03bbf860a0b9d3709fab2e0a77bc2fe9ea26dc04
raghad baizouk <raghad.baizouk@expentech.com>
2024-05-30 00:12:13 +0400

    portfolio

---
+++ src/Data.jsx
@@ -0,0 +1,419 @@
+import Project1 from "./assets/project1.png";
+import Project2 from "./assets/project2.png";
+import Projectf1 from "./assets/projectf1.png";
+import Projectf2 from "./assets/projectf2.png";
+import Projectf3 from "./assets/projectf3.png";
+import Projectf4 from "./assets/projectf4.png";
+import Projectf5 from "./assets/projectf5.png";
+import Projectf11 from "./assets/projectf11.png";
+import Project3 from "./assets/project3.png";
+import Project4 from "./assets/project4.png";
+import Project5 from "./assets/project5.png";
+import Project6 from "./assets/project6.png";
+import Project7 from "./assets/project7.png";
+import Project8 from "./assets/project8.png";
+import Project9 from "./assets/project9.png";
+
+
+export const links = [
+  {
+    name: "Home",
+    path: "home",
+  },
+
+  {
+    name: "Skills",
+    path: "skills",
+  },
+  {
+    name: "Works",
+    path: "work",
+  },
+  {
+    name: "Resume",
+    path: "resume",
+  },
+  {
+    name: "Contact",
+    path: "contact",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    name: "front Development",
+    title: "I have +2 years Angular experience ",
+    description:
+      "Transforming the design into interfaces, starting from scratch. ",
+  },
+  {
+    id: 2,
+    name: "front Development",
+    title: "I have +1 year React experience ",
+    description:
+      "Transforming the design into interfaces, starting from scratch and deploy projects on the server ",
+
+      
+  },
+
+  {
+    id: 3,
+    name: "analysis",
+    title: "I have +1 year Database experience && Oracle procedures  ",
+    description:
+      "The possibility of analyzing the system and collecting work requirements from the customer accurately and Writing procedures in Oracle and dealing with it ERP systems.",
+  },
+  {
+    id: 4,
+    name: " Back Development ",
+    title: " I have +1 year asp.net experience ",
+    description:
+      "create Rest API",
+  },
+ 
+
+];
+
+export const skills = [
+  {
+    id: 1,
+    name: "React",
+    percentage: 90,
+    
+  },
+
+  {
+    id: 2,
+    name: "Angular ",
+    percentage: 50,
+    
+  },
+  {
+    id: 3,
+    name: "Vue ",
+    percentage: 50,
+    
+  },
+  {
+    id: 4,
+    name: "Laravel ",
+    percentage: 70,
+    
+  },
+
+  {
+    id: 5,
+    name: "JavaScript/TypeScript",
+    percentage: 90,
+    
+  },
+
+ 
+
+  {
+    id: 6,
+    name: "Bootstrap",
+    percentage: 85,
+    
+  },
+
+  {
+    id: 7,
+    name: "Tailwind css/Material UI",
+    percentage: 65,
+    
+  },
+  {
+    id: 8,
+    name: "Wordpress",
+    percentage: 65,
+   
+  },
+  {
+    id: 9,
+    name: "Redux, Redux Toolkit",
+    percentage: 65,
+   
+  },
+  {
+    id: 10,
+    name: "ReactNative",
+    percentage: 50,
+    
+  },
+  {
+    id: 11,
+    name: "Oracle, My SQL, SQL",
+    percentage: 65,
+    
+  },
+ 
+  {
+    id: 12,
+    name: "Git",
+    percentage: 90,
+   
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    img: Projectf1,
+    category: "Laravel / Vue",
+    title: "Wadeema",
+    path: "https://wadeema.com",
+    description:
+      "ِpublish ads Dashboard for Alkhaaldi with userName: info@syria5.com and password: admin123",
+    description1: "Number of Font Team: 1",
+    year: "2024",
+  },
+  {
+    id: 2,
+    img: Projectf2,
+    category: "Laravel",
+    title: "Ecommerce",
+    path: "https://v22e.com/",
+    description:
+      "Ecommerce with Dashboard to control in content",
+    description1: "Number of Font Team: 2",
+    year: "2024",
+  },
+  {
+    id: 3,
+    img: Projectf3,
+    category: "Laravel",
+    title: "Ecommerce",
+    path: "https://e11c.com/",
+    description:
+      "Ecommerce with Dashboard to control in content",
+    description1: "Number of Font Team: 2",
+    year: "2024",
+  },
+  {
+    id: 4,
+    img: Projectf4,
+    category: "Wordpress",
+    title: "Ecommerce",
+    path: "https://secret-selection.com/",
+    description:
+      "Clothes Ecommerce",
+    description1: "Number of Font Team: 1",
+    year: "2024",
+  },
+  {
+    id: 5,
+    img: Projectf5,
+    category: "Wordpress",
+    title: "Alkhaaldi",
+    path: "https://alkhaaldi.ae/",
+    description:
+      "Website for our company",
+    description1: "Number of Font Team: 1",
+    year: "2023",
+  },
+  {
+    id: 6,
+    img: Project3,
+    category: "Angular",
+    title: "Mahrocat Company ERP",
+    path: "https://dev03.tatweer.sy/3isadcop/",
+    description:
+      "A project to manage Syria fuels with username:Admin and password:123",
+    description1: "Number of Font Team: 3",
+    year: "2020/2021",
+  },
+ 
+
+  {
+    id: 7,
+    img: Project5,
+    category: "Angular",
+    title: "Distinction and Creativity Agency",
+    path: "https://dev02.tatweer.sy/dcaf/",
+    description:
+      "A project to issue grades for middle school students with username:admin and password:tatweer.",
+      description1:
+      'Number of Font Team: 5',
+      year: "2021",
+  },
+  {
+    id: 8,
+    img: Project6,
+    category: "React",
+    title: "IJK",
+    path: "https://ijk-front.expentech.com",
+    description:
+      "A Project for booking Trips in UAE with username:admin and password:admin123 (it was the first training project in React).",
+      description1:
+      'Number of Font Team: 2',
+      year: "2021",
+  },
+  {
+    id: 9,
+    img: Project7,
+    category: "React",
+    title: "Lertna",
+    path: "https://lertnafront.expentech.com",
+    description:
+      "A project to issue grades for middle school students with username:0965111076 and password:password.",
+      description1:
+      'Number of Font Team: 2',
+      year: "2022",
+  },
+
+  {
+    id: 10,
+    img: Project9,
+    category: "JavaScript",
+    title: "Robot",
+    path: "https://robot.expentech.com/",
+    description: "A project to connect with a robot to detect rubble",
+    description1:
+    'Number of Font Team: only me',
+    year: "2022",
+  },
+  {
+    id: 11,
+    img: Projectf11,
+    category: "React",
+    title: "Teleios",
+    path: "",
+    description: "Task For Teleios Company(code on github)",
+    description1:
+    'task for me',
+    year: "2024",
+  },
+];
+
+export const cv = [
+  {
+    id: 2,
+    title: "Master in Software and Information Systems Engineering",
+    subtitle: "Damascus University, syria",
+    date: "SEP 2020 - PRESENT",
+    description:
+      "I finished all my courses and I am working on my masters thesis.",
+    category: "education",
+  },
+
+  {
+    id: 3,
+    title: "Bachelor in Software Engineering",
+    subtitle: "AL-Baath University, syria",
+    date: "SEP 2014 - AUG 2019",
+    description: "Top 15 in my class.",
+    category: "education",
+  },
+  {
+    id: 4,
+    title: "Web Developer",
+    subtitle: "Alkhaaldi / TechnoCode",
+    date: "June 2023 - ongoing",
+    description:
+      "Develop APIs for mobile and web applications, integrated with Laravel for backend and Vue.js for dashboard creation.",
+
+    description1:
+      "Develop and maintain code for +1 back-end Ecommerce project using Laravel.",
+
+    description2: "Develop +2 project using WordPress.",
+
+
+    category: "experience",
+  },
+  {
+    id: 5,
+    title: "FrontEnd Developer",
+    subtitle: "EXPentech solution, syria",
+    date: "July 2022 - May 2023",
+    description:
+      "Designed UI & integrated code for +4 front-end projects using React.",
+
+    description1:
+      "Designed UI for +2 projects using pure HTML5,CSS3,Bootstrap.",
+
+    description2: "Write clean code, fix bugs and solve problems.",
+    description3: "Collaborate with design team to create user interfaces.",
+    description4: "Deploy projects on the server.",
+
+    category: "experience",
+  },
+
+  {
+    id: 6,
+    title: "Teaching Assistant",
+    subtitle: "IT university, syria",
+    date: "SEP 2021 - MAR 2023",
+    description: "Teaching c++ language.",
+    description1: "Teaching java language.",
+    description2: "Supervisor for +3 IT projects.",
+    category: "experience",
+  },
+
+  {
+    id: 7,
+    title: " Software Developer",
+    subtitle: "Tatweer, syria",
+    date: "July 2020 - July 2022",
+    description:
+      "Designed UI & integrated code for +3 front-end projects using Angular.",
+    description1:
+      "Developed and maintained code for +1 back-end project using Asp.net and Oracle.",
+    description2:
+      "Communicate with customer, collect and discuss requirements.",
+    category: "experience",
+  },
+];
+
+export const photos = [
+  {
+    title:"project1",
+    src: Project2,
+    width: 4,
+    height: 3
+  },
+  {
+    src: Project2,
+    width: 1,
+    height: 1
+  },
+  {
+    src: Project2,
+    width: 3,
+    height: 4
+  },
+  {
+    src: Project2,
+    width: 3,
+    height: 4
+  },
+  {
+    src: Project2,
+    width: 3,
+    height: 4
+  },
+  {
+    src: Project2,
+    width: 4,
+    height: 3
+  },
+  {
+    src: Project2,
+    width: 3,
+    height: 4
+  },
+  {
+    src: Project2,
+    width: 4,
+    height: 3
+  },
+  {
+    src: Project2,
+    width: 4,
+    height: 3
+  }
+];
+
+
+
